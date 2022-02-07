c2d8f7796f7c0290196dc8c53b045670e1b1f879
hjy <5336772+huangtobe@user.noreply.gitee.com>
2022-02-07 13:53:45 +0800

    修改版本号

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,7 @@ import {translate} from './main';
 const { Command } = require('commander');
 const program = new Command();
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .name('fy')
   .usage('<English>')
   .arguments('<English>')
